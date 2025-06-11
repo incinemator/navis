ad7b759b7dc9ade3d45c3edd95f5503becf6de21
incinemator <giannakzorz@gmail.com>
2025-06-11 18:45:48 +0200

    additional import

--- navis_description/launch/display.launch.py
+++ navis_description/launch/display.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import command, PathJoinSubstitution
 import os
 
 def generate_launch_description():
