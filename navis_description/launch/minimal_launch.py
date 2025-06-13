2bc5a8054e4a8bd1ff444166282f60be5381adbd
incinemator <giannakzorz@gmail.com>
2025-06-13 17:17:45 +0200

    code

---
+++ navis_description/launch/minimal_launch.py
@@ -0,0 +1,16 @@
+# minimal_launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{
+                'robot_description': os.popen('xacro /absolute/path/to/navis.urdf.xacro').read()
+            }]
+        )
+    ])
